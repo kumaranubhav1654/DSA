1bf6f363a06466de2748ad2bb81c64456e9ea39e
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-06-15 13:20:58 +0530

    Time: 4 ms (59.81%) | Memory: 30.2 MB (7.24%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        stack<int>st;
-        vector<int> ans(nums.size(), 0);
-        for(int i = nums.size()-1; i!=-1; i--){
-            int tmp = nums[i];
-            if(st.empty())
-                ans[i] = INT_MIN;
-            else{
-                while(!st.empty() && nums[i]>=st.top()) st.pop();
-                if(st.empty()) ans[i] = INT_MIN;
-                else  ans[i] = st.top();
+        int n=nums.size();
+        vector<int> temp(2*n, -1);
+        vector<int> ans;
+        stack<int> st;
+        int count = 2;
+        while(count--){
+            for(int i=n-1;i>=0;i--){
+                if(!st.empty() && st.top()>nums[i]){
+                    temp[count*n +i] = st.top();
+                }else{
+                    while(!st.empty() && st.top()<=nums[i]){
+                        st.pop();
+                    }
+                    if(!st.empty())
+                        temp[count*n +i] =st.top();
+                    else
+                        temp[count*n +i] = -1;
+                }
+                st.push(nums[i]);
             }
-            //cout<<ans[i]<<" "<<nums[i]<<endl;
-            st.push(tmp);
         }
 
-        for(int i = nums.size()-1; i!=-1; i--){
-            if(ans[i]==INT_MIN){
-                while(!st.empty() && nums[i]>=st.top()) st.pop();
-                if(st.empty()) ans[i] = -1;
-                else  ans[i] = st.top();
-            }
-        }
-
+        for(int i=0;i<n;i++) ans.push_back(temp[i]);
         return ans;
     }
 };
