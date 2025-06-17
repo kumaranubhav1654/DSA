074292d8491b6563029805cbe01d541754760642
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-06-17 15:13:04 +0530

    Time: 3 ms (72.22%) | Memory: 15.6 MB (64.99%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void helper(vector<string>& ans, int n, int open, int close, string tmp) {
-        if (tmp.length() == 2*n){ ans.push_back(tmp); return;}
+        if (tmp.length() == 2*n){ ans.push_back(tmp);}
         
         if(open < n)helper(ans, n, open+1, close, tmp+"(");
         
