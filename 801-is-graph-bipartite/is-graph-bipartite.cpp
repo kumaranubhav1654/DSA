b569f005be93258b9d5c2359db0c21257b5963b3
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-06-18 17:21:34 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (11.26%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,27 +1,58 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        int m = graph.size();
-        vector<int> color(m, -1);
+    bool dfs(int i, vector<vector<int>>& graph, vector<int> &color){
+        vector<int> tmp = graph[i];
 
-        for (int i = 0; i != m; i++) {
-            if (color[i] == -1) {
-                queue<pair<int, int>> q;
-                q.push({i, 1});
-                color[i] = 1;
-                while (!q.empty()) {
-                    auto [node, parentColor] = q.front();
-                    q.pop();
-                    for (int i = 0; i != graph[node].size(); i++) {
-                        if (color[graph[node][i]] == -1) {
-                            q.push({graph[node][i], !parentColor});
-                            color[graph[node][i]] = !parentColor;
-                        } else if (color[graph[node][i]] == parentColor)
-                            return false;
-                    }
-                }
+        for(int j = 0; j!=tmp.size(); j++){
+            if(color[tmp[j]]==-1){
+                color[tmp[j]]=!color[i];
+                if(!dfs(tmp[j], graph, color))return false;
+            }
+            else if(color[tmp[j]]==color[i]){
+                return false;
             }
         }
         return true;
     }
+
+    bool isBipartite(vector<vector<int>>& graph) {
+        int m = graph.size();
+        vector<int> color(m, -1);
+        bool ans = true;
+
+        for(int i = 0; i!=m; i++){
+            if(color[i]==-1){
+                color[i] = 1;
+                ans = dfs(i, graph, color);
+                if(ans == false) break;
+            }
+        }
+        return ans;
+    }
 };
+
+
+// BFS Solution:
+
+// int m = graph.size();
+//         vector<int> color(m, -1);
+
+//         for (int i = 0; i != m; i++) {
+//             if (color[i] == -1) {
+//                 queue<pair<int, int>> q;
+//                 q.push({i, 1});
+//                 color[i] = 1;
+//                 while (!q.empty()) {
+//                     auto [node, parentColor] = q.front();
+//                     q.pop();
+//                     for (int i = 0; i != graph[node].size(); i++) {
+//                         if (color[graph[node][i]] == -1) {
+//                             q.push({graph[node][i], !parentColor});
+//                             color[graph[node][i]] = !parentColor;
+//                         } else if (color[graph[node][i]] == parentColor)
+//                             return false;
+//                     }
+//                 }
+//             }
+//         }
+//  return true;
