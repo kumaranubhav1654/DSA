c1e5a7e9d56b0d5e488b534896955d237892e6d6
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-06-18 22:38:15 +0530

    Time: 39 ms (5.01%) | Memory: 101.7 MB (5.07%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool dfs(int i, vector<int> &vis, vector<int> &path, vector<vector<int>>& pq, vector<vector<int>> &adj, int nc){
+        for(int k = 0; k!=adj[i].size(); k++){
+            if(!vis[adj[i][k]]){
+                vis[adj[i][k]] = 1;
+                path[adj[i][k]] = 1;
+                if(dfs(adj[i][k], vis, path, pq, adj, nc)) return true;
+            }
+            else if(path[adj[i][k]]) return true;
+        }
+        path[i] = 0;  
+        return false;
+    }
+    bool canFinish(int nc, vector<vector<int>>& pq) {
+        vector<vector<int>> adj(nc);                                         
+        vector<int> vis(nc, 0);
+
+        for(int i = 0; i!=pq.size(); i++) adj[pq[i][0]].push_back(pq[i][1]);
+
+        for(int i = 0; i!=nc; i++){
+            vector<int> path(nc, 0);
+            if(!vis[i]){
+                if (dfs(i, vis, path, pq, adj, nc)) return false;
+            }
+        }
+        return true;
+    }
+};
