fd2c59215454556d7ac1ccc3f92cf7a689a9af4e
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-06-20 19:33:46 +0530

    Time: 3 ms (73.51%) | Memory: 19 MB (18.65%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool dfs(int indx, vector<vector<int>>& adj, vector<int>& path, vector<int>& vis, vector<int>& ans) {
+        vis[indx] = 1;
+        path[indx] = 1;
+        
+        for (auto t: adj[indx]) {
+            if (!vis[t]){
+                if(dfs(t, adj, path, vis, ans)) return true; 
+            }
+            else if (vis[t] && path[t])
+                return true;
+        }
+
+        path[indx] = 0;
+        ans.push_back(indx);
+        return false;
+    }
+
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        //v<int> st;
+        vector<int> ans;
+        vector<int> path(numCourses, 0);
+        vector<int> vis(numCourses, 0);
+        vector<vector<int>> adj(numCourses);
+
+        for (auto i : prerequisites)
+            adj[i[0]].push_back(i[1]);
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!vis[i]){
+                if (dfs(i, adj, path, vis, ans)) return {};
+            }
+        }
+
+        // while (!st.empty()) {
+        //     ans.push_back(st.top());
+        //     st.pop();
+        // }
+        // reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
