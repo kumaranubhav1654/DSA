d8d626b3a818c0e994e92bb54a963a0cac83963d
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-06-28 15:57:56 +0530

    Time: 1278 ms (5.00%) | Memory: 103.9 MB (5.04%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int, int>>> adj(n);
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>>pq;
+        vector<int> price(n, INT_MAX);
+        vector<int> stp(n);
+        int ans = INT_MAX;
+
+        for(auto i: flights){
+            adj[i[0]].push_back({i[2], i[1]});
+        }
+
+        pq.push({0,src,-1});
+        price[src] = 0;
+
+        while(!pq.empty()){
+            vector<int>info = pq.top();
+            pq.pop();
+
+            int cost = info[0], city = info[1], stops = info[2];
+            
+            if(city==dst && stops<=k) ans = min(ans, cost);
+
+            for(int i = 0; i!=adj[city].size(); i++){
+                auto [money, nxtcity] = adj[city][i];
+                //cout<<"City "<<city<<" Stops "<<stops<<" cost "<<cost<<endl;
+                if(stops<k && (price[nxtcity] > cost+money || stp[nxtcity] > stops+1)){
+                    pq.push({cost+money, nxtcity, stops+1});
+                    price[nxtcity]=cost+money;
+                    stp[nxtcity] = stops+1;
+                    //cout<<"  Details pushed "<<nxtcity<<"  "<<stops+1<<" "<<price[nxtcity]<<endl;
+                }
+            }
+        }
+        return ans==INT_MAX ? -1 : ans;
+    }
+};
