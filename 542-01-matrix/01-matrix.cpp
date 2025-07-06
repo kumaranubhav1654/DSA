2bbeee411e6630a55d8d20cdbd162d7a14e0e8c7
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-07-06 19:51:05 +0530

    Time: 41 ms (13.15%) | Memory: 38.5 MB (49.31%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,51 +1,39 @@
 class Solution {
 public:
-
-    bool isValid(int x, int y, int m, int n){
-        return ((x>=0 && y>=0) && (x<m && y<n));
-    }
-
-    void bfs(queue<pair<pair<int, int>, int>> &q, vector<vector<int>>& vis, int m, int n, vector<vector<int>>& mat){
-        while(!q.empty()){
-            auto [a, b] = q.front();
-            int x = a.first, y = a.second;
-            q.pop();
-
-            int dx[] = {0,0,-1,1}, dy[]={1,-1,0,0};
-
-            for(int i = 0; i!=4; i++){
-                int newx = x+dx[i], newy = y+dy[i];
-                if(isValid(newx, newy, m, n) && !vis[newx][newy]){
-                    if( mat[newx][newy] == 1 && b==0){
-                        q.push({{newx, newy}, 1});
-                        vis[newx][newy] = 1;
-                    }
-                    else{
-                        q.push({{newx, newy}, b+1});
-                        mat[newx][newy] = b+1;
-                        vis[newx][newy] = 1;
-                    }
-                }
-            }
-
-        }
-    }
+bool isValid(int x, int y, int m, int n){
+  return ((x>=0 && y>=0)&&(x<m && y<n));  
+}
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size();
         vector<vector<int>> vis(m, vector<int>(n, 0));
-        queue<pair<pair<int, int>, int>>q;
+        queue<tuple<int, int, int>> q;
 
-        for(int i = 0; i!=m; i++){
-            for(int j = 0; j!=n; j++){
-                if(mat[i][j] == 0 && !vis[i][j]){
-                    q.push({{i, j}, 0});
+        for (int i = 0; i != m; i++) {
+            for (int j = 0; j != n; j++) {
+                if (!vis[i][j] && mat[i][j] == 0) {
                     vis[i][j] = 1;
+                    q.push({i, j, 0});
                 }
             }
         }
 
-        bfs(q, vis, m, n ,mat);
-
-        return mat;
+        while (!q.empty()) {
+            int s = q.size();
+            while (s--) {
+                auto [x, y, step] = q.front();
+                q.pop();
+                int dx[] = {0, 0, 1, -1}, dy[] = {1, -1, 0, 0};
+                for (int j = 0; j != 4; j++) {
+                    int nx = x + dx[j], ny = y + dy[j];
+                    if (isValid(nx, ny, m, n) && !vis[nx][ny]) {
+                        vis[nx][ny] = 1;
+                        mat[nx][ny] = step + 1;
+                        q.push({nx, ny, step + 1});
+                    }
+                }
+            }
+        }
+    return mat;
     }
-};
+}
+;
