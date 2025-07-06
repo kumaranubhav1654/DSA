98d57f495716a0be26151ea2cc862956efaa08ae
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-07-06 14:25:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (66.38%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,80 +1,25 @@
 class Solution {
 public:
-    int find(int i, vector<int>& parent) {
-        if (parent[i] != i)
-            parent[i] = find(parent[i], parent);
-        return parent[i];
-    }
-
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<int> size(n, 1);
-        vector<int> parent(n);
-        int ans = 0;
-
-        for (int i = 0; i != n; i++)
-            parent[i] = i;
-
-        for (int i = 0; i != n; i++) {
-            for (int j = 0; j != n; j++) {
-                if (isConnected[i][j] == 1) {
-                    int pi = find(i, parent);
-                    int pj = find(j, parent);
-                    if (pi == pj)
-                        continue;
-                    if(size[pi] < size[pj]){
-                        parent[pi] = pj;
-                        size[pj] += size[pi];
-                    } else {
-                        parent[pj] = pi;
-                        size[pi] += size[pj];
-                    }
-                }
+    void dfs(vector<vector<int>>& isConnected, int i, vector<int>& vis, int n) {
+        for (int a = 0; a != n; a++) {
+            if (isConnected[i][a] == 1 && !vis[a]) {
+                vis[a] = 1;
+                dfs(isConnected, a, vis, n);
             }
         }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> vis(n, 0);
+        int count = 0;
 
         for (int i = 0; i != n; i++) {
-            if (parent[i] == i)
-                ans++;
+            if (!vis[i]) {
+                vis[i] = 1;
+                count++;
+                dfs(isConnected, i, vis, n);
+            }
         }
-
-        return ans;
+        return count;
     }
 };
-
-// Using DFS:
-
-// class Solution {
-// public:
-//     void dfs(int node, vector<vector<int>>& adj, vector<int> &vis){
-//         vis[node]=1;
-//         for(auto it: adj[node]){
-//             if(!vis[it])
-//             dfs(it, adj, vis);
-//         }
-//     }
-//     int findCircleNum(vector<vector<int>>& isConnected) {
-//         int v = isConnected.size();
-
-//         vector<vector<int>> adj(v);
-//         for(int i = 0; i !=v; i++){
-//             for(int j = 0; j !=v; j++){
-//                 if(isConnected[i][j]==1 && i!=j){
-//                     adj[i].push_back(j);
-//                     adj[j].push_back(i);
-//                 }
-//             }
-//         }
-
-//         vector<int> vis(v, 0);
-//         int provinces = 0;
-
-//         for(int i = 0; i!=v; i++){
-//             if(vis[i]==0){
-//                 provinces++;
-//                 dfs(i, adj, vis);
-//             }
-//         }
-//         return provinces;
-//     }
-// };
