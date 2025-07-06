68509ac51992fae45b9973d10ce4a499e35517ae
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-07-06 11:44:18 +0530

    Time: 7 ms (46.63%) | Memory: 15.4 MB (38.10%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>> ans;
-        queue<vector<int>> q;
-        int n = graph.size();
-        q.push({0});
-
-        while (!q.empty()) {
-            auto a = q.front();
-            q.pop();
-            int len = a.size();
-            int last = a.back();
-            if (last == n-1) {
-                    ans.push_back(a);
-                    continue;
-            }
-            for (int j = 0; j != graph[last].size(); j++) {
-                vector<int> tmp = a;
-                tmp.push_back(graph[last][j]);
-                q.push(tmp);
-            }
+    vector<vector<int>> ans;
+    void dfs(vector<vector<int>>& graph, vector<int> &tmp, int n){
+        int l = tmp.back();
+        if(l==n-1) ans.push_back(tmp);
+        for(int i = 0; i!=graph[l].size(); i++){
+            tmp.push_back(graph[l][i]);
+             dfs(graph, tmp, n);
+             tmp.pop_back();
         }
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<int> tmp;
+        tmp.push_back({0});
+        dfs(graph, tmp, graph.size());
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+//         vector<vector<int>> ans;
+//         queue<vector<int>> q;
+//         int n = graph.size();
+//         q.push({0});
+
+//         while (!q.empty()) {
+//             auto a = q.front();
+//             q.pop();
+//             int len = a.size();
+//             int last = a.back();
+//             if (last == n-1) {
+//                     ans.push_back(a);
+//                     continue;
+//             }
+//             for (int j = 0; j != graph[last].size(); j++) {
+//                 vector<int> tmp = a;
+//                 tmp.push_back(graph[last][j]);
+//                 q.push(tmp);
+//             }
+//         }
+//         return ans;
+//     }
+// };
