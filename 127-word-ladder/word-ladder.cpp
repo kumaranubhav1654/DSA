8e5a6dd9ac8b3d4d38f9463a227806116f3b1481
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-07-09 18:57:14 +0530

    Time: 340 ms (29.74%) | Memory: 25.3 MB (19.56%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        int ans = 1;
-        set<string>st;
-        for(auto i : wordList) st.insert(i);
-        queue<string>q;
+    int helper(string beginWord, string endWord, vector<string>& wordList,
+               set<string> st, int ans) {
+        queue<string> q;
         q.push(beginWord);
 
-        while(!q.empty()){
-            int s = q.size();
-            while(s--){
-                auto a = q.front();
+        while (!q.empty()) {
+            int size = q.size();
+            ans++;
+            while (size--) {
+                string tmp = q.front();
                 q.pop();
-                string orig = a;
-                if(a==endWord) return ans;
-                for(int i = 0; i!=a.size(); i++){
-                    char o = a[i];
-                    for(char c = 'a'; c<='z'; c++){
-                        a[i] = c;
-                        if(st.find(a)!= st.end() && a!=orig){
-                             q.push(a);
-                             st.erase(a);
+                if (tmp == endWord)
+                    return ans;
+                for (int a = 0; a != tmp.length(); a++) {
+                    char origional = tmp[a];
+                    for (char i = 'a'; i <= 'z'; i++) {
+                        tmp[a] = i;
+                        if (st.find(tmp) != st.end()) {
+                            q.push(tmp);
+                            st.erase(tmp);
                         }
                     }
-                    a[i] = o;
+                    tmp[a] = origional;
                 }
             }
-            ans++;
         }
         return 0;
+    }
+    int ladderLength(string beginWord, string endWord,
+                     vector<string>& wordList) {
+        set<string> st;
+        queue<string> q;
+
+        for (auto i : wordList)
+            st.insert(i);
+
+        return helper(beginWord, endWord, wordList, st, 0);
     }
 };
