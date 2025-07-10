3fc3da63fcb9f7f6870d5c29003b067fb11b6861
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-07-10 09:39:19 +0530

    Time: 38 ms (27.49%) | Memory: 24.7 MB (68.34%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    bool isValid(int i, int j, int n) {
-        return ((i >= 0 && j >= 0) && (i < n && j < n));
+    bool isValid(int i , int j, int n){
+        return ((i>=0 && j>=0) && (j<n && i<n));
     }
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        if (grid[0][0] == 1)
-            return -1;
-        priority_queue<pair<int, pair<int, int>>,
-                       vector<pair<int, pair<int, int>>>,
-                       greater<pair<int, pair<int, int>>>> pq;
-        
         int n = grid.size();
-        
+        if(grid[0][0]!=0) return -1;
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>>pq;
         vector<vector<int>> vis(n, vector<int>(n, -1));
-
-        int dx[] = {0, 0, -1, -1, -1, 1, 1, 1},
-            dy[] = {-1, 1, -1, 0, 1, -1, 0, 1};
-
-        pq.push({1, {0, 0}});
-
-        while (!pq.empty()) {
-            auto [dis, add] = pq.top();
+        pq.push({1, 0, 0});
+        vis[0][0] = 1;
+        
+        while(!pq.empty()){
+            auto [dis, i, j] = pq.top();
             pq.pop();
-            int i = add.first, j = add.second;
-            if (i == n - 1 && j == n - 1)
-                return dis;
-            for (int a = 0; a != 8; a++) {
-                int ni = i + dx[a], nj = j + dy[a];
-                if (isValid(ni, nj, n) && grid[ni][nj] == 0 && vis[ni][nj]==-1){
-                    pq.push({dis + 1, {ni, nj}});
-                    vis[ni][nj] = 1;
+
+            if(i==n-1 && j==n-1) return dis;
+
+            int dx[]= {0,1,1,1,0,-1,-1,-1}, dy[]={1,1,0,-1,-1,-1,0,1};
+            for(int a = 0; a!=8; a++){
+                int newI= i+dx[a], newJ= j+dy[a];
+                if(isValid(newI, newJ, n) && vis[newI][newJ] == -1 && grid[newI][newJ]==0){
+                    pq.push({dis+1, newI, newJ});
+                    vis[newI][newJ] = 1;
                 }
             }
+
         }
         return -1;
     }
