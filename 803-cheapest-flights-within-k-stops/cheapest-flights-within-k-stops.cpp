fe804e39221249a2f988a8dab64e4513d3f4a471
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-07-10 17:37:59 +0530

    Time: 31 ms (7.22%) | Memory: 20 MB (10.22%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
         vector<vector<pair<int, int>>> adj(n);
-        queue<vector<int>>pq;
-        vector<int> price(n, INT_MAX);
-        vector<int> stp(n);
-        int ans = INT_MAX;
 
-        for(auto i: flights){
-            adj[i[0]].push_back({i[2], i[1]});
+        for (int i = 0; i != flights.size(); i++) {
+            adj[flights[i][0]].push_back({flights[i][2], flights[i][1]});
         }
 
-        pq.push({0,src,-1});
-        price[src] = 0;
+        int ans = INT_MAX;
 
-        while(!pq.empty()){
-            vector<int>info = pq.front();
+        vector<pair<int, int>> cities(n, {INT_MAX, INT_MAX});
+
+        pq.push({0, 0, src});
+
+        while (!pq.empty()) {
+            auto [cost, stopsTaken, currCity] = pq.top();
             pq.pop();
 
-            int cost = info[0], city = info[1], stops = info[2];
-            
-            if(city==dst && stops<=k) ans = min(ans, cost);
+            if (currCity == dst) ans = min(ans, cost);
 
-            for(int i = 0; i!=adj[city].size(); i++){
-                auto [money, nxtcity] = adj[city][i];
-                if(stops<k && (price[nxtcity] > cost+money || stp[nxtcity] > stops+1)){
-                    pq.push({cost+money, nxtcity, stops+1});
-                    price[nxtcity]=cost+money;
-                    stp[nxtcity] = stops+1;
+            for (int i = 0; i != adj[currCity].size(); i++) {
+                auto [price, desCity] = adj[currCity][i];
+
+                if(cities[desCity].first < price+cost && cities[desCity].second < stopsTaken) continue;
+                else{
+                    cities[desCity].first = min(cities[desCity].first,  price+cost);
+                    cities[desCity].second = min (cities[desCity].second, stopsTaken+1);
+                }
+
+                if(stopsTaken <= k){
+                    pq.push({cost + price, stopsTaken + 1, desCity});
                 }
             }
         }
-        return ans==INT_MAX ? -1 : ans;
+        return (ans == INT_MAX) ? -1 : ans;
     }
 };
