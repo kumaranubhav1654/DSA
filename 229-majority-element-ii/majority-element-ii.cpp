a7f123c8372e66703c3ff558277c63bd8a140c20
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-07-20 00:30:35 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (78.97%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -5,37 +5,36 @@ public:
         int num1 = nums[0], num2 = nums[0];
         int count1 = 1, count2 = 0;
 
-        for (int i = 1; i != size; i++) {
-            if (num1 == nums[i])
+        for(int i = 1; i!=size; i++){
+            if(num1==nums[i])
                 count1++;
-            else if (num2 == nums[i])
+            
+            else if(num2==nums[i])
                 count2++;
-            else {
-
-                if (count1 == 0) {
+            
+            else{
+                if(count1==0) {
                     num1 = nums[i];
-                    count1 = 1;
-                } else if (count2 == 0) {
-                    num2 = nums[i];
-                    count2 = 1;
-                } else {
+                    count1=1;
+                }
+                else if(count2==0){
+                     num2 = nums[i];
+                     count2=1;
+                }
+                else{
                     count1--;
-                    count2--;
+                count2--;
                 }
             }
         }
-        count1 = 0, count2 = 0;
-        for (int i = 0; i != size; i++) {
-            if (num1 == nums[i])
-                count1++;
-            else if (num2 == nums[i])
-                count2++;
+        count1=0, count2=0;
+        for(int i = 0; i!=size; i++){
+            if(num1==nums[i])count1++;
+            else if(num2==nums[i])count2++;
         }
         vector<int> ans;
-        if (count1 > size / 3)
-            ans.push_back(num1);
-        if (count2 > size / 3 && num1 != num2)
-            ans.push_back(num2);
+        if(count1>size/3) ans.push_back(num1);
+        if(count2>size/3 && num1 != num2) ans.push_back(num2);
         return ans;
     }
 };
