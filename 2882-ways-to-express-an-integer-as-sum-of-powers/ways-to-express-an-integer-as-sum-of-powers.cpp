3ecde545d6384ed6cac8c00b6c51046591489035
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-08-12 18:29:22 +0530

    Time: 179 ms (54.90%) | Memory: 73.5 MB (51.85%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     const int MOD = 1e9 + 7;
-    int helper(int num, int n, int x, vector<vector<int>>& dp) {
+    int helper(int p, int n, int x, vector<vector<int>>& dp) {
         if (n < 0)
             return 0;
-        if (num == 0) {
+        if (p == 0) {
             if (n == 0)
                 return 1;
             return 0;
         }
-        if (dp[num][n] != -1)
-            return dp[num][n];
-        int tmp = pow(num, x);
+        if (dp[p][n] != -1)
+            return dp[p][n];
+        int tmp = pow(p, x);
 
-        int take = helper(num - 1, n - tmp, x, dp);
-        int notTake = helper(num - 1, n, x, dp);
+        int take = helper(p - 1, n - tmp, x, dp);
+        int notTake = helper(p - 1, n, x, dp);
 
-        dp[num][n] = (take + notTake) % MOD;
-        return dp[num][n];
+        dp[p][n] = (take + notTake) % MOD;
+        return dp[p][n];
     }
     int numberOfWays(int n, int x) {
-        int ans = 0, tmp = n;
-        int num = ceil(pow(tmp, 1.0 / x));
-        if (num <= 1)
+        int p = ceil(pow(n, 1.0 / x));
+        if (p <= 1)
             return 1;
-        vector<vector<int>> dp(num + 1, vector<int>(n + 1, -1));
-        return helper(num, tmp, x, dp);
+        vector<vector<int>> dp(p + 1, vector<int>(n + 1, -1));
+        return helper(p, n, x, dp);
     }
 };
