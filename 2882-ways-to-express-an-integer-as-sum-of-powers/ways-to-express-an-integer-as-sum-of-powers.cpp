701bbd0107df5122f3f811bfb6ae24fa9efee878
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-08-12 18:24:48 +0530

    Time: 159 ms (59.26%) | Memory: 73.6 MB (50.11%) - LeetSync

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+    int helper(int num, int n, int x, vector<vector<int>>& dp) {
+        if (n < 0)
+            return 0;
+        if (num == 0) {
+            if (n == 0)
+                return 1;
+            return 0;
+        }
+        if (dp[num][n] != -1)
+            return dp[num][n];
+        int tmp = pow(num, x);
+        // cout<<" tmp "<<tmp<<" "<<n;
+
+        int take = helper(num - 1, n - tmp, x, dp);
+        int notTake = helper(num - 1, n, x, dp);
+
+        dp[num][n] = ((long long)take + notTake) % MOD;
+        return dp[num][n];
+    }
+    int numberOfWays(int n, int x) {
+        int ans = 0, tmp = n;
+        int num = ceil(pow(tmp, 1.0 / x));
+        // cout<<"NUM "<<num<<endl;
+        if (num <= 1)
+            return 1;
+        vector<vector<int>> dp(num + 1, vector<int>(n + 1, -1));
+        return helper(num, tmp, x, dp);
+    }
+};
