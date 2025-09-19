550cb42d2c52e3aa0e74eb1195696f19ba3fca13
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-09-19 20:03:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (18.59%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
-bool isValid(int x,int y,int m, int n){
-return ((x>=0 && y>=0)&&(x<m && y<n));
-}
     int orangesRotting(vector<vector<int>>& grid) {
-        int ans = -1;
-        int countGood = 0;
         int m = grid.size(), n = grid[0].size();
-        queue<pair< int, int>>q;
+        int goodOranges = 0, step = 0;
+        queue<pair<int, int>>q;
+        vector<vector<int>> visited(m, (vector<int>(n, -1)));
 
         for(int i = 0; i!=m; i++){
             for(int j = 0; j!=n; j++){
-                if(grid[i][j]==2) q.push({i,j});
-                if(grid[i][j]==1) countGood++;
+                if(grid[i][j]==1) goodOranges++;
+                else if (grid[i][j]==2){
+                visited[i][j] = 1;
+                 q.push({i, j});
+                 }
             }
         }
 
-        if(!countGood) return 0;
-
-        while(!q.empty()){
+        int dx[] = {1,-1,0,0}, dy[] = {0,0,-1,1};
+        while(!q.empty() && goodOranges!=0){
             int s = q.size();
             while(s--){
-                auto [ i , j] = q.front();
+                auto [x, y] = q.front();
                 q.pop();
-
-                int dx[] = {0,0,1,-1}, dy[] = {1,-1,0,0};
-                for(int a = 0; a!=4; a++){
-                    int ni = i+dx[a], nj = j+dy[a];
-                    if(isValid(ni, nj, m, n) && grid[ni][nj]==1){
-                        q.push({ ni, nj});
-                        grid[ni][nj]=2;
-                        countGood--;
-                    } 
+                for(int i = 0; i!=4; i++){
+                    int newX = x + dx[i], newY = y + dy[i];
+                    if(newX>=0 && newX<m && newY>=0 && newY<n && grid[newX][newY]==1 && visited[newX][newY]!=1){
+                        grid[newX][newY]=2;
+                        q.push({newX, newY});
+                        goodOranges--;
+                    }
                 }
+ 
             }
-            ans++;
+            step++;
         }
-        if(!countGood)
-        return ans;
+        if(!goodOranges) return step;
         else return -1;
     }
 };
