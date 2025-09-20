1a3cf2c38378d81a78c78895f65eceae857ef1a7
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-09-20 14:22:10 +0530

    Time: 0 ms (100.00%) | Memory: 99.2 MB (71.91%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        int n = INT_MIN;
-        map<int, int> inDeg;
+        // int n = INT_MIN;
+        // map<int, int> inDeg;
 
-        for(int i = 0; i!=edges.size(); i++){
-            inDeg[edges[i][0]]++;
-            inDeg[edges[i][1]]++;
-            n = max(n, max(edges[i][1], edges[i][0]));
-        }
+        // for(int i = 0; i!=edges.size(); i++){
+        //     inDeg[edges[i][0]]++;
+        //     inDeg[edges[i][1]]++;
+        //     n = max(n, max(edges[i][1], edges[i][0]));
+        // }
 
-        for(auto itr = inDeg.begin(); itr!=inDeg.end(); itr++ ){
-            if(itr->second==n-1) return itr->first;
-        }
+        // for(auto itr = inDeg.begin(); itr!=inDeg.end(); itr++ ){
+        //     if(itr->second==n-1) return itr->first;
+        // }
+
+        int n1 = edges[0][1], n2 = edges[0][0];
+        if(n1==edges[1][1] || n1==edges[1][0]) return n1;
+        else return n2;
+
         return -1;
     }
 };
