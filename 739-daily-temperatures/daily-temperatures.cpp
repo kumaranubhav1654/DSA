158a39965243254c4fed57a2e3689a4640f02ece
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-10-16 23:56:17 +0530

    Time: 33 ms (19.13%) | Memory: 102.9 MB (66.44%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -6,18 +6,10 @@ public:
         stack<pair<int, int>>st;
 
         for(int i = size-1; i!=-1; i--){
-            if(st.empty())
-                ans[i] = 0;
-            
-            else if (st.top().first > temp[i])
-                ans[i] = 1;
-            
-            else{
-                while(!st.empty() && st.top().first<=temp[i]) st.pop();
-                if(st.empty())
-                    ans[i]=0;
-                else ans[i] = st.top().second - i;
-            }
+            while(!st.empty() && st.top().first<=temp[i]) st.pop();
+            if(st.empty())ans[i] = 0;
+            else ans[i] = st.top().second - i;
+
             st.push({temp[i], i});
         }
         return ans;
