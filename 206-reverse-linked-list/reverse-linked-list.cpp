f46299190ecef21247e3951a7d7f1a36957f9713
Kumar Anubhav <54079549+kumaranubhav1654@users.noreply.github.com>
2025-12-27 16:05:36 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (89.93%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -8,18 +8,39 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
+ //public: This method is wrong as we are not reversing the link but the values.Also extra space.
+//     ListNode* ans = new ListNode(0);
+//     ListNode* ans1 = ans;
+//     void helper(ListNode* head){
+//         if(head == NULL) return;
+//         helper(head->next);
+//         ans->next = new ListNode(head->val);
+//         ans=ans->next;
+//     }
+//     ListNode* reverseList(ListNode* head) {
+//         helper(head);
+//         return ans1->next;
+//     }
+// };
+
+
 class Solution {
 public:
-    ListNode* ans = new ListNode(0);
-    ListNode* ans1 = ans;
-    void helper(ListNode* head){
-        if(head == NULL) return;
-        helper(head->next);
-        ans->next = new ListNode(head->val);
-        ans=ans->next;
-    }
     ListNode* reverseList(ListNode* head) {
-        helper(head);
-        return ans1->next;
+        if(!head || !head->next) return head;
+
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while(curr){
+            ListNode* tmp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+
+        return prev;
     }
 };
